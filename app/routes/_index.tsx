7b4410412448cc2b7e2fa1bca366764cd409b609
fahmi <fahmi@192.168.4.58>
2023-07-07 18:08:35 +0700

    feat(components): add button components

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,23 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import type { V2_MetaFunction } from '@remix-run/node'
+import { Button } from '~/components/ui/button'
 
 export const meta: V2_MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+	return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }]
+}
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+	return (
+		<div className="flex gap-4 p-4" style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+			<Button variant="default">Primary</Button>
+			<Button size="sm" variant="danger">
+				Danger
+			</Button>
+			<Button size="lg" variant="outline">
+				Outline
+			</Button>
+			<Button variant="secondary">Secondary</Button>
+			<Button variant="ghost">Ghost</Button>
+			<Button variant="link">Link</Button>
+		</div>
+	)
 }
